35fe48c0310060863a3c87a46a62334d6c8634dd
adnbr <aidan.dunbar@googlemail.com>
2013-08-31 23:29:31 +0100

    Readme & Code
    Moved to 10 bit ADC result. Smoother output. 1023 mapped to 255 using
    Arduino map function, see comments for link.
    All data filtered now, some stepping while changing values, not an
    issue.  Now more stable in the lower ranges, won't jitter as easily when
    on the bounds of off segment.
    Now phase correct PWM.

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-microscope-light-source
+Microscope Light Source
 =======================
+
+A simple PWM capable constant current source for a high brightness LED
+based around an ATtiny13 and an LM317 voltage regulator. 
+
+The board has been designed to be primarily one-sided, it should be
+possible to etch it DIY. This hasn't been tested though. The top copper
+layer only has a ground fill and the extra unused pins brought out to
+pads for possible use at a later date. 
+
+You can find out more [here](http://bit.ly/1d51tgB).
+
+Mentioned [here](http://bit.ly/17b90Ci).
--- code/main.c
+++ code/main.c
@@ -5,24 +5,35 @@
  * PB1/OC0B: LED output (Pin 6)
  * PB2/ADC1: Potentiometer input (Pin 7)
  * 
- * Find out more: 
- * http://www.adnbr.co.uk/articles/upgrading-a-to-led-illumination-pt-1
+ * Find out more: http://bit.ly/1d51tgB
  * -------------------------------------------------------------------*/
 
 // 9.6 MHz, built in resonator
 #define F_CPU 9600000
+
+#define true 1
+#define false 0
  
 #include <avr/io.h>
+#include <util/delay.h>
 
 // Define led ext output pin on PB2
 #define LED PB1 
+// Point on the scale (0-1023) that corresponds to "OFF" on the knob.
+#define OFF_BELOW 100
 
-#define OFF_BELOW 20
-
-int led_brightness;
+// Map function from Arduino project: http://bit.ly/18taXex
+long map(long x, long i_min, long i_max, long o_min, long o_max)
+{
+  return (x - i_min) * (o_max - o_min) / (i_max - i_min) + o_min;
+}
 
 int main(void)
 {
+    int brightness = 0;
+    int mapped = 0; 
+    int adc_in;
+   
     // LED is an output.
     DDRB |= (1 << LED);
     
@@ -31,13 +42,10 @@ int main(void)
     
     // Set ADC prescaler to clock/128.
     // At 9.6 MHz this is 75 kHz.
-    ADCSRA |= (1 << ADPS2) | (1 << ADPS1) | (1 << ADPS0);
+    ADCSRA |= (1 << ADPS1) | (1 << ADPS0) | (1 << ADEN);
     
-    // ADC input is PB2/ADC1. || Only use ADCH, essential an 8 bit ADC.
-    ADMUX |= (1 << MUX0) | (1 << ADLAR);
-    
-    // ADC is in auto-triggering free-running mode. || ADC is enabled.
-    ADCSRA |= (1 >> ADATE) | (1 << ADEN);
+    // ADC input is PB2/ADC1.
+    ADMUX |= (1 << MUX0);
 
     // Start ADC conversions.
     ADCSRA |= (1 << ADSC); 
@@ -45,31 +53,45 @@ int main(void)
     // PWM setup code
     // -----------------------------------------------------------------
     
-    // Set Timer 0 prescaler to clock/64.
-    // At 9.6 MHz this is 150 kHz.
-    TCCR0B |= (1 << CS01) | (1 << CS00);
+    // Set Timer 0 prescaler to clock/8.
+    TCCR0B |= (1 << CS01) | (1<<CS00);
     
-    // Set to 'Fast PWM' mode
-    TCCR0A |= (1 << WGM01) | (1 << WGM00);
+    // Set to 'Phase Correct PWM' mode
+    TCCR0A |= (1 << WGM02) | (1 << WGM00);
 
-    TCCR0A |= (1 << COM0B1) | (1 << COM0B0);
+    TCCR0A |= (1 << COM0B1);
     
     // Set PWM to 0 brightness.
     OCR0B = 0;
     
     // Infinite loop
     while (1) {
-        // Get the latest ADC reading
-        led_brightness = ADCH;
         
-        // We want an "off" position to supplement the physical off
-        // power switch we added. This range roughly matches the "off" 
-        // position on the original intensity control knob.
-        if (led_brightness <= OFF_BELOW) {
-            led_brightness = 0;
+        // Wait for the previous conversion to finish
+        while (ADCSRA & (1 << ADSC));
+        // Start a conversion
+        ADCSRA |= (1 << ADSC);
+        
+        // Get the 10 bit ADC value
+        adc_in = ADC;
+       
+        // Smooth the original input to stop jumping around.
+        // ADC input is quite noisy, needs calming down.
+        brightness = (brightness - (brightness / 4)) + (adc_in / 4);
+ 
+        // There is a section of the original knob that is marked "OFF"
+        // so if the pot is in that section, turn off.
+        // The rest of the time, remap the pot so that we still have a 
+        // low brightness setting.
+        if (brightness < OFF_BELOW) {
+            OCR0B = 0;
+        } else {
+            // Smooth out any rounding issues caused by a moving ADC
+            // value and mapping.
+            mapped = (mapped - (mapped / 4)) + 
+                     (map(brightness, OFF_BELOW, 1023, 0, 255) / 4);
+            OCR0B = mapped;
         }
-        
-        // Update the PWM.
-        OCR0B = led_brightness;
+      
     }
 }
