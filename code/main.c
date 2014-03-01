1a48f999080d722c53b37deb0e1510ff2c6d3e61
adnbr <git@adnbr.co.uk>
2014-03-01 13:23:40 +0000

    Rewritten, easier to read, use, etc.
    8-bit ADC directly to PWM. dedicated setup/read/write functions for ADC
    and PWM.
    
    Clearer comments for bit-wrangling sections of the setup (mentions
    appropriate tables in datasheet for future reference.)

--- code/main.c
+++ code/main.c
@@ -1,5 +1,6 @@
 /* ---------------------------------------------------------------------
  * PWM LED Brightness control for ATtiny13.
+ * Datasheet for ATtiny13: http://www.atmel.com/images/doc2535.pdf
  * 
  * Pin configuration -
  * PB1/OC0B: LED output (Pin 6)
@@ -10,88 +11,82 @@
 
 // 9.6 MHz, built in resonator
 #define F_CPU 9600000
-
-#define true 1
-#define false 0
- 
-#include <avr/io.h>
-#include <util/delay.h>
-
-// Define led ext output pin on PB2
 #define LED PB1 
-// Point on the scale (0-1023) that corresponds to "OFF" on the knob.
-#define OFF_BELOW 100
 
-// Map function from Arduino project: http://bit.ly/18taXex
-long map(long x, long i_min, long i_max, long o_min, long o_max)
+// Point on the scale (0-255) that corresponds to "OFF" on the knob.
+#define OFF_BELOW 25
+
+#include <avr/io.h>
+
+void adc_setup (void)
 {
-  return (x - i_min) * (o_max - o_min) / (i_max - i_min) + o_min;
+    // Set the ADC input to PB2/ADC1, left align result
+    ADMUX |= (1 << MUX0) | (1 << ADLAR);
+
+    // Set the prescaler to clock/128 & enable ADC
+    // At 9.6 MHz this is 75 kHz.
+    // See ATtiny13 datasheet, Table 14.4.
+    ADCSRA |= (1 << ADPS1) | (1 << ADPS0) | (1 << ADEN);
+
+    // Start the first conversion
+    ADCSRA |= (1 << ADSC);
 }
 
-int main(void)
+int adc_read (void)
+{
+    // Get the 8-bit result.
+    int adc_result = ADCH;
+
+    // Start the next conversion
+    ADCSRA |= (1 << ADSC);
+
+    return adc_result;
+}
+
+void pwm_setup (void)
+{
+    // Set Timer 0 prescaler to clock/8.
+    // At 9.6 MHz this is 1.2 MHz.
+    // See ATtiny13 datasheet, Table 11.9.
+    TCCR0B |= (1 << CS01);
+
+    // Set to 'Fast PWM' mode
+    TCCR0A |= (1 << WGM01) | (1 << WGM00);
+
+    // Clear OC0B output on compare match, upwards counting.
+    TCCR0A |= (1 << COM0B1);
+}
+
+void pwm_write (int val)
+{
+    OCR0B = val;
+}
+
+int main (void)
 {
     int brightness = 0;
-    int mapped = 0; 
-    int adc_in;
-   
+    int adc_in = 0;
+
     // LED is an output.
-    DDRB |= (1 << LED);
-    
-    // ADC setup code
-    // -----------------------------------------------------------------
-    
-    // Set ADC prescaler to clock/128.
-    // At 9.6 MHz this is 75 kHz.
-    ADCSRA |= (1 << ADPS1) | (1 << ADPS0) | (1 << ADEN);
-    
-    // ADC input is PB2/ADC1.
-    ADMUX |= (1 << MUX0);
+    DDRB |= (1 << LED);  
 
-    // Start ADC conversions.
-    ADCSRA |= (1 << ADSC); 
-    
-    // PWM setup code
-    // -----------------------------------------------------------------
-    
-    // Set Timer 0 prescaler to clock/8.
-    TCCR0B |= (1 << CS01) | (1<<CS00);
-    
-    // Set to 'Phase Correct PWM' mode
-    TCCR0A |= (1 << WGM02) | (1 << WGM00);
+    adc_setup();
+    pwm_setup();
 
-    TCCR0A |= (1 << COM0B1);
-    
-    // Set PWM to 0 brightness.
-    OCR0B = 0;
-    
-    // Infinite loop
+    pwm_write(0);
+  
     while (1) {
-        
-        // Wait for the previous conversion to finish
-        while (ADCSRA & (1 << ADSC));
-        // Start a conversion
-        ADCSRA |= (1 << ADSC);
-        
-        // Get the 10 bit ADC value
-        adc_in = ADC;
-       
+        // Get the ADC value
+        adc_in = adc_read();
+
         // Smooth the original input to stop jumping around.
         // ADC input is quite noisy, needs calming down.
         brightness = (brightness - (brightness / 4)) + (adc_in / 4);
- 
-        // There is a section of the original knob that is marked "OFF"
-        // so if the pot is in that section, turn off.
-        // The rest of the time, remap the pot so that we still have a 
-        // low brightness setting.
-        if (brightness < OFF_BELOW) {
-            OCR0B = 0;
+
+		if (brightness < OFF_BELOW) {
+            pwm_write(0);
         } else {
-            // Smooth out any rounding issues caused by a moving ADC
-            // value and mapping.
-            mapped = (mapped - (mapped / 4)) + 
-                     (map(brightness, OFF_BELOW, 1023, 0, 255) / 4);
-            OCR0B = mapped;
+            pwm_write(brightness);
         }
-      
     }
 }
